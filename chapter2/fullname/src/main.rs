d6edc1ab9747b72cbdfb4e6cbe14415fb7137987
natsuokawai <kawai.natsuo@gmail.com>
2021-03-21 19:08:32 +0900

    Add validate_name function to FullName

--- chapter2/fullname/src/main.rs
+++ chapter2/fullname/src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use regex::Regex;
 
 fn main() {
     let name = FullName::new(String::from("Natsuo"), String::from("Kawai"));
@@ -26,9 +27,19 @@ impl fmt::Display for FullName {
 
 impl FullName {
     fn new(first_name: String, last_name: String) -> Self {
+        FullName::validate_name(&first_name);
+        FullName::validate_name(&last_name);
+
         FullName {
             first_name,
             last_name,
+        }
+    }
+
+    fn validate_name(name: &str) {
+        let re = Regex::new(r"^[a-zA-Z]+$").unwrap();
+        if !re.is_match(name) {
+            panic!("name contains invalid characters.");
         }
     }
 }
