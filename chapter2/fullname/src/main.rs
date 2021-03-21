b1992e0668a95b7418f7e7441b83ef424db1c60e
natsuokawai <kawai.natsuo@gmail.com>
2021-03-21 18:54:26 +0900

    Add FullName object

---
+++ chapter2/fullname/src/main.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+
+fn main() {
+    let name = FullName::new(String::from("Natsuo"), String::from("Kawai"));
+    println!("name.first_name: {}", name.first_name);
+    println!("name.last_name : {}", name.last_name);
+
+    let name2 = FullName::new(String::from("Natsuo"), String::from("Kawai"));
+    println!("name2.first_name: {}", name2.first_name);
+    println!("name2.last_name : {}", name2.last_name);
+
+    println!("name == name2: {}", name == name2);
+}
+
+#[derive(PartialEq)]
+struct FullName {
+    first_name: String,
+    last_name: String,
+}
+
+impl fmt::Display for FullName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.first_name, self.last_name)
+    }
+}
+
+impl FullName {
+    fn new(first_name: String, last_name: String) -> Self {
+        FullName {
+            first_name,
+            last_name,
+        }
+    }
+}
